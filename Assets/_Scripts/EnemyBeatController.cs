234e89eac9112ccf2b4da64bb33ee643ac834a60
Dmitrii Puntusov <puntusovdima@gmail.com>
2024-12-13 16:37:41 +0100

    Added Goblin animations to the Animations/Goblin folder

--- Assets/_Scripts/EnemyBeatController.cs
+++ Assets/_Scripts/EnemyBeatController.cs
@@ -1,22 +1,20 @@
 using UnityEngine;
 
-namespace _Scripts
+
+public class EnemyBeatController : MonoBehaviour, ITriggerEnter 
 {
-    public class EnemyBeatController : MonoBehaviour, ITriggerEnter 
-    {
-        private enum CharacterState { Chase,Attack, Hurt, WaitToAttack, Death}
+    private enum CharacterState { Chase, Attack, Hurt, WaitToAttack, Death}
 
-        [SerializeField] private float minTimeBeforeAttack, minDistanceToAttack, maxTimeBeforeAttack;
+    [SerializeField] private float minTimeBeforeAttack, minDistanceToAttack, maxTimeBeforeAttack;
     
-        private CharacterState _state;
+    private CharacterState _state;
 
-        private Rigidbody2D _rb;
-        private Vector2 _movement;
-        private Animator _anim;
-        private Transform _target;
-        public void HitByPlayer(GameObject player)
-        {
-            throw new System.NotImplementedException();
-        }
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    private Animator _anim;
+    private Transform _target;
+    public void HitByPlayer(GameObject player)
+    {
+        throw new System.NotImplementedException();
     }
 }
