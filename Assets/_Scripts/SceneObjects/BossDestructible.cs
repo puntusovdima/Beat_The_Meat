45d42601b47de2487d795a5f871b8172d9c593d5
Enrique Iglesias <ike.umafi@gmail.com>
2025-01-05 18:57:45 +0100

    Boss Arena Created

--- Assets/_Scripts/SceneObjects/BossDestructible.cs
+++ Assets/_Scripts/SceneObjects/BossDestructible.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BossDestructible : MonoBehaviour, ITriggerEnter
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject cam;
     [SerializeField] private GameObject runeGlow;
     [SerializeField] private GameObject pillarShadow;
 
-    private Vector3 bossRoomPos = new Vector3(10, 0, 0);
+    private Vector3 bossRoomPlayerPos = new Vector3(120, 0, 0);
+    private Vector3 bossRoomCamPos = new Vector3(123, 0, -10);
 
     public void HitByPlayer(GameObject player)
     {
@@ -28,6 +30,9 @@ public class BossDestructible : MonoBehaviour, ITriggerEnter
         yield return new WaitForSeconds(0.5f);
         //Take the player to the boss room or spawn the boss
         Debug.Log("HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE");
-        player.transform.position = bossRoomPos;
+        player.transform.position = bossRoomPlayerPos;
+        cam.transform.position = bossRoomCamPos;
+        
+
     }
 }
